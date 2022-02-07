15b1b3ce1fda36cad7660c0f0563210e0fda2fd6
Ben Borgers <borgersbenjamin@gmail.com>
2022-02-06 19:35:26 -0500

    headless: true on Puppeteer

--- scrape.js
+++ scrape.js
@@ -6,7 +6,7 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({
     args: ["--no-sandbox"],
-    headless: false,
+    headless: true,
   });
 
   const page = await browser.newPage();
