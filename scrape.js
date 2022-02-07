cde8a1103c4a89aab3202e94a670e9d95caedbff
Ben Borgers <borgersbenjamin@gmail.com>
2022-02-06 19:34:14 -0500

    Add scrape.js

---
+++ scrape.js
@@ -0,0 +1,29 @@
+// This file scrapes the JumboCash portal for how many meal swipes I’ve used.
+const JUMBOCASH_PASSWORD = process.env.JUMBOCASH_PASSWORD;
+
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: false,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto("https://www.jumbocash.net/login.php");
+  await page.waitForSelector("#loginphrase");
+  await page.type("#loginphrase", "1370784");
+  await page.type("#password", JUMBOCASH_PASSWORD);
+  await page.click('input[type="submit"]');
+  await page.waitForSelector(".jsa_transactions"); // Wait for account page to load.
+
+  const rawSwipes = await page.evaluate(
+    () => document.querySelector("table:last-of-type tbody .sr-only").innerText
+  );
+  const swipes = parseInt(rawSwipes.replace("Current Balance", "").trim());
+  console.log(`${swipes} swipes left`);
+
+  await page.close();
+  await browser.close();
+})();
