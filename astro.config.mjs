13060105b7e6bf544e6e6fad2f8690edbb2d575e
Ben Borgers <borgersbenjamin@gmail.com>
2022-12-24 23:25:09 -0800

    Init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,4 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-import react from "@astrojs/react"; // @ts-check
-
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig(
-  /** @type {import('astro').AstroUserConfig} */
-  {
-    base: "/swipes",
-    integrations: [react(), tailwind()],
-  }
-);
+export default defineConfig({});
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{astro,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
